ca5ac43c2d5dfa48c9e8b26a7a79af6750de7691
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2025-10-05 16:50:23 +0530

    feat: add callModelAPI module

--- src/utils/callModelAPI.js
+++ src/utils/callModelAPI.js
@@ -1,4 +1,5 @@
 import { Client } from "@gradio/client";
+import { ApiError } from "./ApiError.js";
 
 const callModelAPI = async (videoUrl, age, gender) => {
 	try {
@@ -9,57 +10,21 @@ const callModelAPI = async (videoUrl, age, gender) => {
 			age,
 			gender,
 		});
-		console.log({ result });
+
+		if (result.success) {
+			throw new ApiError({
+				message: result.message,
+				errors: [result.error],
+				statusCode: 500,
+				data: result,
+			});
+		}
 
 		return result;
 	} catch (error) {
 		console.log(error);
-
 		throw error;
 	}
 };
 
 export { callModelAPI };
-
-// import { ApiError } from "./ApiError.js";
-
-// const callModelAPI = async (videoUrl, age, gender) => {
-// 	try {
-// 		const response = await fetch(
-// 			"https://nitingupta9967-hr-prediction.hf.space/run/predict",
-// 			{
-// 				method: "POST",
-// 				headers: {
-// 					"Content-Type": "application/json",
-// 				},
-// 				body: JSON.stringify({
-// 					data: [videoUrl, age, gender],
-// 				}),
-// 			},
-// 		);
-
-// 		if (!response.ok) {
-// 			throw new ApiError({
-// 				message: "HTTP error while calling model API",
-// 				statusCode: response.status,
-// 			});
-// 		}
-
-// 		const result = await response.json();
-
-// 		if (result.error) {
-// 			throw new ApiError({
-// 				message: "Model API error",
-// 				statusCode: 500,
-// 				errors: [result.error],
-// 			});
-// 		}
-
-// 		return result;
-// 	} catch (error) {
-// 		console.log(error);
-// 		throw error;
-// 	}
-// };
-
-// export { callModelAPI };
